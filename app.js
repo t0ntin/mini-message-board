46e92fe05395d58cab7db7a8e23802a634623908
t0ntin <115113636+t0ntin@users.noreply.github.com>
2025-08-25 20:32:23 -0500

    Added post route to index.js.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, '/public')));
-
+app.use(express.urlencoded({extended:true}));
 // routes
 app.use('/', router);
 
--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,16 @@ router.get('/new', (req, res) => {
   res.render('form',  { title: "Add a new message"});
 });
 
+router.post('/new', (req, res) => {
+  const { user, text } = req.body;
+
+  messages.push({
+    text: text,
+    user: user,
+    added: new Date()
+  });
+
+  res.redirect('/');
+});
+
 export default router;
